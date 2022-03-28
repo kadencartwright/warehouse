79aa7da19faed77ea2d017cb85da01284dee87c9
kadencartwright <cartwrightkaden@gmail.com>
2022-03-28 18:17:55 -0500

    addedsome missing imports for recently created resources

--- apps/server/src/gear-record-photos/gear-record-photos.module.ts
+++ apps/server/src/gear-record-photos/gear-record-photos.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { GearRecordPhotosService } from "./gear-record-photos.service";
 import { GearRecordPhotosResolver } from "./gear-record-photos.resolver";
 import { gearRecordPhotosProviders } from "./gear-record-photos.providers";
+import { DatabaseModule } from "src/database/database.module";
 
 @Module({
   providers: [
@@ -9,5 +10,6 @@ import { gearRecordPhotosProviders } from "./gear-record-photos.providers";
     GearRecordPhotosService,
     ...gearRecordPhotosProviders,
   ],
+  imports: [DatabaseModule],
 })
 export class GearRecordPhotosModule {}
--- apps/server/src/gear-records/gear-records.module.ts
+++ apps/server/src/gear-records/gear-records.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { GearRecordsService } from "./gear-records.service";
 import { GearRecordsResolver } from "./gear-records.resolver";
 import { gearRecordsProviders } from "./gear-records.providers";
+import { DatabaseModule } from "src/database/database.module";
 
 @Module({
   providers: [GearRecordsResolver, GearRecordsService, ...gearRecordsProviders],
+  imports: [DatabaseModule],
 })
 export class GearRecordsModule {}
